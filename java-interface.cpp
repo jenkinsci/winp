5e01054bdada763c2c4e84089456d54f51cf7bfa
kohsuke <null>
2008-02-04 06:07:53 +0000

    renamed

--- java-interface.cpp
+++ java-interface.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "java-interface.h"
-#include "kill-tree.h"
+#include "winp.h"
 
 JNIEXPORT jboolean JNICALL Java_org_jvnet_winp_Native_kill(JNIEnv* env, jclass clazz, jint pid, jboolean recursive) {
 	return KillProcessEx(pid,recursive);
