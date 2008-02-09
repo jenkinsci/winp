e0fff473c3883d79e09b8c0a9ab405b43de1da52
kohsuke <null>
2008-02-09 03:24:03 +0000

    added new winp.dll

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -8,7 +8,7 @@ import org.jvnet.winp.Priority;
 public class Main {
     public static void main(String[] args) {
         WinProcess p = new WinProcess(Integer.parseInt(args[0]));
-        // p.killRecursively();
-        p.setPriority(Priority.BELOW_NORMAL);
+        p.killRecursively();
+        // p.setPriority(Priority.BELOW_NORMAL);
     }
 }
