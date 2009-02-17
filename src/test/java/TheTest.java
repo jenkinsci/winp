fc1260a086244e42012123d919efbf96f9a1a184
kohsuke <null>
2009-02-17 08:40:30 +0000

    improved test

--- src/test/java/TheTest.java
+++ src/test/java/TheTest.java
@@ -32,14 +32,18 @@ public class TheTest extends TestCase {
     }
 
     public void testKill() throws Exception {
-        Process p = Runtime.getRuntime().exec("notepad");
+        ProcessBuilder pb = new ProcessBuilder("notepad");
+        pb.environment().put("TEST","foobar");
+        Process p = pb.start();
         try {
             WinProcess wp = new WinProcess(p);
             System.out.println("pid="+wp.getPid());
             
             System.out.println(wp.getCommandLine());
             assertTrue(wp.getCommandLine().contains("notepad"));
+
             System.out.println(wp.getEnvironmentVariables());
+            assertEquals("foobar",wp.getEnvironmentVariables().get("TEST"));
 
             Thread.sleep(100);
             new WinProcess(p).killRecursively();
