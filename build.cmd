5d56f7a1e7064f295957044db415c3bac3a39d0e
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-03-03 12:50:50 +0100

    Draft implementation of build.cmd and appveyor.yml

---
+++ build.cmd
@@ -0,0 +1,60 @@
+@echo off
+setlocal
+set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\12.0\Bin\;
+set BUIDROOT=%cd%
+
+:getopts
+if "%1"=="" (goto :default) else (goto :%1)
+goto :exit
+
+:default
+goto :cleanbuild
+
+:cleanbuild
+echo "### Cleaning the build directory"
+cd %BUIDROOT%\native
+msbuild winp.vcxproj /t:Clean /p:Configuration=Release /verbosity:minimal /nologo /p:Platform="Win32"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild winp.vcxproj /t:Clean /p:Configuration=Release /verbosity:minimal /nologo /p:Platform="x64"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild winp.vcxproj /t:Clean /p:Configuration=Debug /verbosity:minimal /nologo /p:Platform="Win32"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild winp.vcxproj /t:Clean /p:Configuration=Debug /verbosity:minimal /nologo /p:Platform="x64"
+if %errorlevel% neq 0 exit /b %errorlevel%
+mvn clean
+if %errorlevel% neq 0 exit /b %errorlevel%
+goto :build
+
+:build
+echo "### Building project configurations"
+cd %BUIDROOT%\native
+REM /verbosity:minimal
+msbuild winp.vcxproj /p:Configuration=Release /nologo /p:Platform="Win32"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild winp.vcxproj /p:Configuration=Release /nologo /p:Platform="x64"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild winp.vcxproj /p:Configuration=Debug /nologo /p:Platform="Win32"
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild winp.vcxproj /p:Configuration=Debug /nologo /p:Platform="x64"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "### Updating WinP resource files"
+cd %BUIDROOT%
+COPY native\Release\winp.dll src\main\resources\winp.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+COPY native\x64\Release\winp.dll src\main\resources\winp.x64.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo "### Build WinP"
+mvn package
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+goto :exit
+
+:test
+mvn verify
+if %errorlevel% neq 0 exit /b %errorlevel%
+goto :exit
+
+:exit
+endlocal
