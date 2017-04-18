38213d22f5daacdfa86b23f87482badf8cddc8a0
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-04-18 12:54:03 +0200

    Build Debuggable JARs as a part of the CI build (#36)
    * Build.cmd: Support parameterization of version/configuration to enhance the Appveyor build flow
    
    * Pom.xml: Explicitly define latest Maven package versions in the build flow
    
    * Explicitly define build version in Appveyor
    
    * Fix the Appveyor Definition
    
    * Split build commands to make mvn version:set work reliably
    
    * Make version definition more reliably in build.cmd
    
    * Try to publish JUnit reports to Appveyor
    
    * Revert "Try to publish JUnit reports to Appveyor"
    
    This reverts commit 209aa699913cbd61c8553bfba6e840fafc8dbc6a.

--- build.cmd
+++ build.cmd
@@ -4,6 +4,23 @@ set PATH=%PATH%;%ProgramFiles(x86)%\MSBuild\12.0\Bin\;
 set BUIDROOT=%cd%
 
 :getopts
+if "%2"=="" (
+	set configuration="Release"
+) else (
+	set configuration=%2%
+)
+
+if "%3"=="" (
+	echo No target version specified, will determine it from POM
+	REM TODO: Apply some MADSKILLZ to do it without the temporary file?
+	call mvn -q -Dexec.executable="cmd.exe" -Dexec.args="/c echo ${project.version}" --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec > version.txt
+	for /f "delims=" %%x in (version.txt) do set version=%%x
+) else (
+	echo Setting MVN project version to the externally defined %3%
+	set version=%3%	
+)
+echo Target version is %version%
+
 if "%1"=="" (goto :default) else (goto :%1)
 goto :exit
 
@@ -11,41 +28,35 @@ goto :exit
 goto :cleanbuild
 
 :cleanbuild
-echo "### Cleaning the build directory"
+echo ### Cleaning the %configuration% build directory
 cd %BUIDROOT%\native
-msbuild winp.vcxproj /t:Clean /p:Configuration=Release /verbosity:minimal /nologo /p:Platform="Win32"
+msbuild winp.vcxproj /t:Clean /p:Configuration=%configuration% /verbosity:minimal /nologo /p:Platform="Win32"
 if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild winp.vcxproj /t:Clean /p:Configuration=Release /verbosity:minimal /nologo /p:Platform="x64"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild winp.vcxproj /t:Clean /p:Configuration=Debug /verbosity:minimal /nologo /p:Platform="Win32"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild winp.vcxproj /t:Clean /p:Configuration=Debug /verbosity:minimal /nologo /p:Platform="x64"
+msbuild winp.vcxproj /t:Clean /p:Configuration=%configuration% /verbosity:minimal /nologo /p:Platform="x64"
 if %errorlevel% neq 0 exit /b %errorlevel%
 goto :build
 
 :build
-echo "### Building project configurations"
+echo ### Building the %configuration% configuration
 cd %BUIDROOT%\native
 REM /verbosity:minimal
-msbuild winp.vcxproj /p:Configuration=Release /nologo /p:Platform="Win32"
+msbuild winp.vcxproj /p:Configuration=%configuration% /nologo /p:Platform="Win32"
 if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild winp.vcxproj /p:Configuration=Release /nologo /p:Platform="x64"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild winp.vcxproj /p:Configuration=Debug /nologo /p:Platform="Win32"
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild winp.vcxproj /p:Configuration=Debug /nologo /p:Platform="x64"
+msbuild winp.vcxproj /p:Configuration=%configuration% /nologo /p:Platform="x64"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo "### Updating WinP resource files"
+echo ### Updating WinP resource files for the %configuration% build
 cd %BUIDROOT%
-COPY native\Release\winp.dll src\main\resources\winp.dll
+COPY native\%configuration%\winp.dll src\main\resources\winp.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
-COPY native\x64\Release\winp.dll src\main\resources\winp.x64.dll
+COPY native\x64\%configuration%\winp.dll src\main\resources\winp.x64.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo "### Build and Test winp.jar"
+echo ### Build and Test winp.jar for %version%
 cd %BUIDROOT%
-mvn --batch-mode clean package verify
+call mvn -q --batch-mode versions:set -DnewVersion=%version%
+if %errorlevel% neq 0 exit /b %errorlevel%
+call mvn --batch-mode clean package verify 
 if %errorlevel% neq 0 exit /b %errorlevel%
 goto :exit
 
